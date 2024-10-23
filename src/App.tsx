f697aaf2b7fc9d87d6f5f095db9bbefc159f223b
jantal80 <98295344+jantal80@users.noreply.github.com>
2024-10-23 09:57:39 -0400

    update version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { JaaButton, JaaTable } from 'jaa-my-component-library';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
